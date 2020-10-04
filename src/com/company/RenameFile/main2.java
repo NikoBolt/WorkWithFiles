6b250e0d514c121987fe9b036b46e0bb086e5fa5
NikoBolt <docnikbolt@gmail.com>
2020-10-04 18:33:35 +0300

    переделал

--- src/com/company/RenameFile/main2.java
+++ src/com/company/RenameFile/main2.java
@@ -1,7 +1,6 @@
 package com.company.RenameFile;
 
 import java.io.File;
-import java.util.HashMap;
 
 public class main2 {
 
@@ -12,51 +11,64 @@ public class main2 {
     //    ...
     //    399.jpg - 82.jpg
     //    400.jpg - 81.jpg
+    //    ...
+    //    399.jpg - 82.jpg
+    //    480.jpg - 001.jpg
 
     public static void main(String[] args) {
         String path1 = "M:\\1";     //path1 указывает на директорию c файлами
         String path2 = "M:\\2\\";   //path2 куда будут перемещены переименованные файлы
 
         int start = 40;     // номер с которого начинается нумерация файлов
-        int stop = 100;     // на файле с каким номером закончить
+//        int stop = 480;     // на файле с каким номером закончить
+        int stop = 441;     // на файле с каким номером закончить
 
-        HashMap<Integer, File> map = new HashMap<Integer, File>();
+//        int first = 40;
+        int newFirst = 441;
+//        int n1 = String.valueOf(start).length();
+//        int n2 = String.valueOf(stop).length();
+//        int n1 = 3; // количество знаков в числе
+        int n = String.valueOf(newFirst).length();
+
+        int a;
 
         String st1 = "";
         String st2 = "";
+        String name = "";
+
+        StringBuilder sb = new StringBuilder();
+
 
         File dir = new File(path1);
+        File newFile;
+
         for ( File file : dir.listFiles()) {
-            st1 = file.getName().substring(0, 3);
+            name = file.getName();
+            st1 = name.split(" ")[0];
+
+            int k = start + stop - Integer.valueOf(st1);
+
             if ( file.isFile() ) {
-                map.put(Integer.valueOf(st1),file);
+                System.out.println(st1);
+                a = String.valueOf(k).length();
+                while (a++ < n) {
+                    sb.append("0");
+                }
+                sb.append(k);
+
+//                st2 = String.valueOf(start + stop - Integer.valueOf(st1));
+                System.out.println(sb);
+
+                newFile = new File(path2 + name.replace(st1, sb));
+
+                if (file.renameTo(newFile)) {
+                    System.out.println("Файл " + name + " перемещен успешно");
+                } else {
+                    System.out.println("Файл " + name + " не был перемещен");
+                }
             }
+            sb = new StringBuilder();
         }
 
-        for (int i = start; i < stop+1; i++) {
-            if (i<10) {
-                st1 = "00" + i;
-                st2 = "" + (481 - i);
-            } else if (i<100) {
-                st1 = "0" + i;
-                st2 = "" + (481 - i);
-            } else if (i < 1000) {
-                st1 = "" + i;
-                st2 = "" + (481 - i);
-            }
-
-            File file = map.get(i);
-            File newFile = new File(path2 + map.get(i).getName().replace(st1 , st2));
-
-            System.out.println(map.get(i).getName());
-            System.out.println(st1);
-            System.out.println(st2);
-
-            if (file.renameTo(newFile)) {
-                System.out.println("Файл перемещен успешно");
-            } else {
-                System.out.println("Файл не был перемещен");
-            }
-        }
     }
 }

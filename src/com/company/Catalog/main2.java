8a3e60bdbe0ceb63bcdb93b1227c5b600f37333f
NikoBolt <docnikbolt@gmail.com>
2020-10-12 23:02:02 +0300

    +

--- src/com/company/Catalog/main2.java
+++ src/com/company/Catalog/main2.java
@@ -1,16 +1,21 @@
 package com.company.Catalog;
 
-import java.io.*;
+//import java.io.*;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class main2 {
     public static void main(String[] args) {
-        String path1 = "M:\\1\\";       //path1 указывает на директорию c файлами
+        String path1 = "U:\\1\\";       //path1 указывает на директорию c файлами
         String path2 = "src\\com\\company\\Catalog\\xxx.txt";   // Куда записывать
 
         String pre = "";
 
-        ArrayList<String> name1 = new ArrayList<String>();
         File dir = new File(path1);
         try(FileOutputStream outfile = new FileOutputStream(path2)) {
             BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outfile));
@@ -27,18 +32,18 @@ public class main2 {
     }
 
     public static void readDir(File dir, BufferedWriter bufferedWriter, String pre) throws IOException {
-        ArrayList<String> name1 = new ArrayList<String>();
+        ArrayList<String> files = new ArrayList<String>();
 
-        for (File file : dir.listFiles()) {
-            if (file.isFile()){
-                name1.add(file.getName());
+        for (File file : dir.listFiles()) {     // Проверяем содержимое папки
+            if (file.isFile()){                 // Если это файл, то сохраняем в ArrayList
+                files.add(file.getName());
             }
-            if (!file.isFile()) {
+            if (!file.isFile()) {               // Если это папка, то рекурсия
                 readDir(file, bufferedWriter, pre + file.getName() + "\\");
-            }
+            }                                   // Выйдя имеем в файле всё содержимое папки
         }
-        for (int i = 0; i < name1.size(); i++) {
-            bufferedWriter.write(pre + name1.get(i) + "\n");
+        for (int i = 0; i < files.size(); i++) {    // и дописываем туда файлы
+            bufferedWriter.write(pre + files.get(i) + "\n");
         }
     }
 }

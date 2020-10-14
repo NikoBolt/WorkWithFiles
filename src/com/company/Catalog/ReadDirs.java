23bbf273311424a0399b199477de7a117c86123a
NikoBolt <docnikbolt@gmail.com>
2020-10-14 18:38:46 +0300

    3 поведения: 1. чтение в память; 2. запись из памяти и 3. чтение-запись.

--- src/com/company/Catalog/ReadDirs.java
+++ src/com/company/Catalog/ReadDirs.java
@@ -11,24 +11,39 @@ import java.io.*;
 import java.util.ArrayList;
 
 public class ReadDirs {
+    private String path = "";
+    private String out = "";
+    private String pre = "";
+    private StringBuilder sb = new StringBuilder();
+
     public ReadDirs(String path, String out) {
+        this.path = path;
+        this.out = out;
+        pre = path;
+    }
+    public ReadDirs(String path, String out, String pre) {
+        this(path, out);
+        this.pre = pre;
+    }
+
+    public String readPackage() {
         File dir = new File(path);
-        try(FileOutputStream outfile = new FileOutputStream(out)) {
-            BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outfile));
-            readDir(dir, bufferedWriter, "");
-            bufferedWriter.flush();
+        try {
+            readDir(dir, sb, pre);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return sb.toString();
     }
-    public ReadDirs(String path, String out, String pre) {
-//        pre = path;
-        File dir = new File(path);
+
+    public void writePaskage() {
         try(FileOutputStream outfile = new FileOutputStream(out)) {
             BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outfile));
-            readDir(dir, bufferedWriter, pre);
+
+            bufferedWriter.write(sb.toString());
+
             bufferedWriter.flush();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -37,6 +52,39 @@ public class ReadDirs {
         }
     }
 
+    public void readWritePackage() {
+        File dir = new File(path);
+        try(FileOutputStream outfile = new FileOutputStream(out)) {
+            BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outfile));
+
+            readDir(dir, bufferedWriter, pre);
+
+            bufferedWriter.flush();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Из файла в память
+    private void readDir(File dir, StringBuilder sb, String pre) throws IOException {
+        ArrayList<String> files = new ArrayList<String>();
+
+        for (File file : dir.listFiles()) {     // Проверяем содержимое папки
+            if (file.isFile()){                 // Если это файл, то сохраняем в ArrayList
+                files.add(file.getName());
+            }
+            if (!file.isFile()) {               // Если это папка, то рекурсия
+                readDir(file, sb, pre + file.getName() + "\\");
+            }                                   // Выйдя имеем в файле всё содержимое папки
+        }
+        for (int i = 0; i < files.size(); i++) {    // и дописываем туда файлы
+            sb.append(pre + files.get(i) + "\n");
+        }
+    }
+
+    // из файла в файл
     private void readDir(File dir, BufferedWriter bufferedWriter, String pre) throws IOException {
         ArrayList<String> files = new ArrayList<String>();
 
@@ -51,5 +99,10 @@ public class ReadDirs {
         for (int i = 0; i < files.size(); i++) {    // и дописываем туда файлы
             bufferedWriter.write(pre + files.get(i) + "\n");
         }
+    }
+
+    public String toString() {
+        if (sb.toString().equals("")) return "Сначала надо прочитать содержимое директории";
+        return sb.toString();
     }
 }
--- src/com/company/Catalog/ReadDirsTest.java
+++ src/com/company/Catalog/ReadDirsTest.java
@@ -2,9 +2,24 @@ package com.company.Catalog;
 
 public class ReadDirsTest {
     public static void main(String[] args) {
-        String path1 = "U:\\1\\";       //path1 указывает на директорию c файлами
+        String path1 = "M:\\1\\";       //path1 указывает на директорию c файлами
         String path2 = "src\\com\\company\\Catalog\\xxx.txt";   // Куда записывать
 
-        new ReadDirs(path1,path2,path1);
+        ReadDirs rd3 = new ReadDirs(path1,path2,path1);
+        System.out.println(rd3.toString());
+
+        // Чтение директории в память
+        // и запись из памяти в файл
+        ReadDirs rd = new ReadDirs(path1,path2,path1);
+        rd.readPackage();
+        System.out.println(rd.toString());
+        rd.writePaskage();
+
+        // Чтение директории и запись в файл
+        ReadDirs rd2 = new ReadDirs(path1,path2,path1);
+        rd2.readWritePackage();
+        System.out.println(rd2.toString()); // в памяти ничего нет
+
+
     }
 }
